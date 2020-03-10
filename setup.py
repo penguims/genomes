456672ba4c632da429115fab518f66391eb29e01
Magic Fang <43994576+penguims@users.noreply.github.com>
2020-03-10 08:07:07 +0800

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,20 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2020,  Magic Fang, magicfang@gmail.com
+#
+# Distributed under terms of the GPL-3 license.
+
+from setuptools import setup, find_packages
+
+setup(
+    name = 'seqman',
+    version = 0.1,
+    packages = find_packages(),
+    author = 'Magic Fang',
+    author_email = 'magicfang@gmail.com',
+    url = 'https://github.com/penguims/genomes',
+    license = 'https://www.gnu.org/licenses/quick-guide-gplv3.en.html',
+    description = 'Genomics sequence manipulating toolset and packages'
+    ) 
