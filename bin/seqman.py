3a24a7a164f657b244b09c400970aaf9b91b0d8a
Magic Fang <43994576+penguims@users.noreply.github.com>
2020-03-10 22:24:13 +0800

    Add files via upload

--- bin/seqman.py
+++ bin/seqman.py
@@ -7,6 +7,7 @@
 # Distributed under terms of the GPL-3 license.
 
 import sys;
+sys.path.append("/Users/magic/Downloads/genomes-master");
 from Bio import SeqIO;
 from seqman import argparser, getfh, close;
 from seqman.consts import *;
@@ -16,21 +17,28 @@ from seqman.seqfilter import seqfilter;
 parser = argparser(True);
 args = parser.parse_args();
 cmd = commands(args.command, args);
-fil = seqfilter(args);
 if not cmd:
-	parser.error("command not found!");
+	parser.print_help();
+	parser.exit("command not found!");
+fil = seqfilter(args);
+if args.show:
+	showitem(args);
+	exit(0);
+
+#Initial output file format, default is input file format
+if not args.oformat:
+	args.oformat = args.format;
 
 # ifh: Input file handle, ofh: Output file handle
 (ifh, ofh) = (args.infile, args.output);
 ifh = getfh(args.infile, "rt");
 ofh = getfh(args.output, "wt");
 if not ifh or not ofh:
-	parser.error("input or output file error");
+	parser.parse_args([args.command, "-h"]);
+#	parser.exit("no input file!");
 
-#Initial output file format, default is input file format
-if not args.oformat:
-	args.oformat = args.format;
 
+ocseq = "";
 for seq in SeqIO.parse(ifh, args.format, alphabet = ALPHABET[args.alphabet]):
 	if args.upper or args.lower:
 		seq = fil.upperlower(seq);
@@ -39,7 +47,11 @@ for seq in SeqIO.parse(ifh, args.format, alphabet = ALPHABET[args.alphabet]):
 	cseq = cmd.run(seq);
 	if cseq:
 		if args.command == "split":
-			SeqIO.write(seq, cseq, args.oformat);
+			if not ocseq or ocseq != cseq:
+				close(ofh);
+				ocseq = cseq;
+				ofh = getfh(ocseq, "wt");
+			SeqIO.write(seq, ofh, args.oformat);
 		elif args.command == "stat" or args.command == "translate" and args.six:
 			ofh.write(cseq);
 		else:
